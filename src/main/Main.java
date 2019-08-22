fac93271c33304edb050e9e423d0139e1f5c0c9f
This PC <parakh.tiwari_cs18@gla.ac.in>
2019-08-22 15:13:23 +0530

    accept input from the user

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,17 @@
 package main;
+
+import java.util.Scanner;
+
 /**
  * @author parakhtiwari
  * @version 1.0
  */
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner val = new Scanner(System.in);
-        String s;
-        System.out.println("Enter the name");
-        s = val.next();
-        System.out.println("welcome, " + s);
+        String userName = "";
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine().trim();
+        scanner.close();
+
     }
 }
